f926594cfab601112e615f369194ac01d9c0d953
marcosguilhermef <marcosguilhermefh@gmail.com>
2022-08-21 18:50:35 -0300

    auxilio

--- app/Http/Controllers/ConsultarAuxilio.php
+++ app/Http/Controllers/ConsultarAuxilio.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
@@ -10,16 +11,32 @@ class ConsultarAuxilio extends Controller
     private $HTTP = "https://api.portaldatransparencia.gov.br/api-de-dados/auxilio-emergencial-por-cpf-ou-nis";
 
     public function index(Request $request){
+        
+        $page = intval($request->input("pagina"));
+        $data = collect();
+        $response = $this->request($request,$page);
+        do{
+            $response = $this->request($request,$page);
+            $data = $data->merge($response->toArray());
+            $page++;
+        }
+        while(!$response->isEmpty());
+
+        return response()->json($data);
+    }
+
+    private function request(Request $request, $pagina = 1){
         $response = Http::withHeaders(
             [
                 'chave-api-dados' => env('TOKEN_AUXILIO'),
             ]
         )->get($this->HTTP,
             [
-                'pagina'             => $request->input("pagina"),
+                'pagina'             => $pagina,
                 'codigoBeneficiario' => $request->input("cpf"),
             ]
         );
-        return $response->json();
+
+        return $response->collect();
     }
 }
