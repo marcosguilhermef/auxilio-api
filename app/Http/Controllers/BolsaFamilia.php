fb0703af2ee0fa26ad9430766773d57690fa3286
marcosguilhermef <marcosguilhermefh@gmail.com>
2022-10-13 19:53:23 -0300

    ajuste bolsa familia

--- app/Http/Controllers/BolsaFamilia.php
+++ app/Http/Controllers/BolsaFamilia.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use DateTime;
 
 class BolsaFamilia extends Controller
 {   
@@ -12,23 +13,45 @@ class BolsaFamilia extends Controller
 
     public function index(Request $request){
        
-        $response = $this->request($request);
-
-        return response()->json($response->toArray());
+        //$response = $this->request($request);
+        //$this->getAuxilio($request);
+        //dd($this->getAuxilio($request));
+        
+        $array = $this->getAuxilio($request);
+        
+        return response()->json($array);
     }
 
-    private function request(Request $request){
+
+    private function getAuxilio($request){
+        $today  = (new DateTime("2021-05"))->format("Ym");
+        $day    = -12;
+        $date   = (new DateTime("2021-05 ".$day." months"))->format("Ym");
+        $data   = collect();
+
+        do{
+            $response = $this->request($request->input("cpf"),$date);
+            $data = $data->merge($response->toArray());
+            $day = $day + 1;
+            $date = (new DateTime("2021-05 ".$day." months"))->format("Ym");
+        }while($day != 0);
+
+        return $data->toArray();
+    }
+
+    private function request($cpf,$date){
         $response = Http::withHeaders(
             [
                 'chave-api-dados' => env('TOKEN_AUXILIO'),
             ]
         )->get($this->HTTP,
             [
-                'codigo' => $request->input("cpf"),
-                'anoMesCompetencia' => $request->input("anoMesCompetencia")
+                'codigo' => $cpf,
+                'anoMesCompetencia' => $date
             ]
         );
 
         return $response->collect();
     }
+
 }
